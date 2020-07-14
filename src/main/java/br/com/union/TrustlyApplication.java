71f78687f9d07168568bd072a0e02eba30004f2e
Jair H Dresch <jhdresch@gmail.com>
2020-07-13 22:01:11 -0300

    first commit

---
+++ src/main/java/br/com/union/TrustlyApplication.java
@@ -0,0 +1,13 @@
+package br.com.union;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/union/TrustlyApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.union;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustlyApplicationTests {
+	
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
